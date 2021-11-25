cf27f359b8955516d4acf68c3296961d900325db
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-11-25 10:03:05 -0500

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,14 @@ function Home() {
     </div>
   );
 }
-
 {/* write an <About> component here */}
+function About(){
+  return(
+    <div id='about'>
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
@@ -24,9 +30,10 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
   );
 }
-
 export default App;
